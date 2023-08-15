ba8a040ee4b001ac364a68d0aa62c465cb9d0a06
fahad-whizpool <124687012+fahad-whizpool@users.noreply.github.com>
2023-08-15 06:32:06 -0700

    french,italian & japinese fields added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: public_member_api_docs
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
@@ -24,23 +22,6 @@ class AdaptiveBanner extends StatefulWidget {
 }
 
 class AdaptiveBannerState extends State<AdaptiveBanner> {
-  // BannerAd? _bannerAd2;
-  // bool _isLoaded2 = false;
-  // BannerAd? _bannerAd1;
-  // bool _isLoaded1 = false;
-  final String _adUnitId = Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/6300978111'
-      : 'ca-app-pub-3940256099942544/2934735716';
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    // _isLoaded1 = false;
-    // _isLoaded2 = false;
-    // _loadAd1();
-    // _loadAd2();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,15 +32,19 @@ class AdaptiveBannerState extends State<AdaptiveBanner> {
           ),
           body: Stack(
             children: [
-              const Align(
+              Align(
                 alignment: Alignment.bottomCenter,
-                child: SafeArea(
-                  child: BannerAdMobContainer(),
+                child: Container(
+                  height: 50,
+                  color: Colors.yellow,
                 ),
               ),
-              const Align(
+              Align(
                 alignment: Alignment.topCenter,
-                child: BannerAdMobContainer(),
+                child: Container(
+                  height: 50,
+                  color: Colors.yellow,
+                ),
               ),
               Center(
                 child: GestureDetector(
@@ -81,108 +66,10 @@ class AdaptiveBannerState extends State<AdaptiveBanner> {
           )),
     );
   }
-
-  /// Loads and shows a banner ad.
-  ///
-  /// Dimensions of the ad are determined by the width of the screen.
-  void _loadAd2() async {
-    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
-    final size = await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
-        MediaQuery.of(context).size.width.truncate());
-
-    if (size == null) {
-      // Unable to get width of anchored banner.
-      return;
-    }
-
-    BannerAd(
-      adUnitId: _adUnitId,
-      request: const AdRequest(),
-      size: size,
-      listener: BannerAdListener(
-        // Called when an ad is successfully received.
-        onAdLoaded: (ad) {
-          // setState(() {
-          //   _bannerAd2 = ad as BannerAd;
-          //   _isLoaded2 = true;
-          // });
-        },
-        // Called when an ad request failed.
-        onAdFailedToLoad: (ad, err) {
-          ad.dispose();
-        },
-        // Called when an ad opens an overlay that covers the screen.
-        onAdOpened: (Ad ad) {},
-        // Called when an ad removes an overlay that covers the screen.
-        onAdClosed: (Ad ad) {},
-        // Called when an impression occurs on the ad.
-        onAdImpression: (Ad ad) {},
-      ),
-    ).load();
-  }
-
-  @override
-  void dispose() {
-    // _bannerAd1?.dispose();
-    // _bannerAd2?.dispose();
-    super.dispose();
-  }
-
-  void _loadAd1() async {
-    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
-    final size = await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
-        MediaQuery.of(context).size.width.truncate());
-
-    if (size == null) {
-      // Unable to get width of anchored banner.
-      return;
-    }
-
-    BannerAd(
-      adUnitId: _adUnitId,
-      request: const AdRequest(),
-      size: size,
-      listener: BannerAdListener(
-        // Called when an ad is successfully received.
-        onAdLoaded: (ad) {
-          // setState(() {
-          //   _bannerAd1 = ad as BannerAd;
-          //   _isLoaded1 = true;
-          // });
-        },
-        // Called when an ad request failed.
-        onAdFailedToLoad: (ad, err) {
-          ad.dispose();
-        },
-        // Called when an ad opens an overlay that covers the screen.
-        onAdOpened: (Ad ad) {},
-        // Called when an ad removes an overlay that covers the screen.
-        onAdClosed: (Ad ad) {},
-        // Called when an impression occurs on the ad.
-        onAdImpression: (Ad ad) {},
-      ),
-    ).load();
-  }
 }
 
-class ScreenB extends StatefulWidget {
+class ScreenB extends StatelessWidget {
   const ScreenB({super.key});
-  @override
-  ScreenBState createState() => ScreenBState();
-}
-
-class ScreenBState extends State<ScreenB> {
-  // BannerAd? _bannerAd2;
-  // bool _isLoaded2 = false;
-  // BannerAd? _bannerAd1;
-  // bool _isLoaded1 = false;
-
-  final String _adUnitId = 'ca-app-pub-3940256099942544/2934735716';
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -192,13 +79,19 @@ class ScreenBState extends State<ScreenB> {
         ),
         body: Stack(
           children: [
-            const Align(
+            Align(
               alignment: Alignment.bottomCenter,
-              child: BannerAdMobContainer(),
+              child: Container(
+                height: 50,
+                color: Colors.yellow,
+              ),
             ),
-            const Align(
+            Align(
               alignment: Alignment.topCenter,
-              child: BannerAdMobContainer(),
+              child: Container(
+                height: 50,
+                color: Colors.yellow,
+              ),
             ),
             Center(
               child: GestureDetector(
@@ -215,87 +108,5 @@ class ScreenBState extends State<ScreenB> {
             )
           ],
         ));
-  }
-
-  /// Loads and shows a banner ad.
-  ///
-  /// Dimensions of the ad are determined by the width of the screen.
-  void _loadAd2() async {
-    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
-    final size = await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
-        MediaQuery.of(context).size.width.truncate());
-
-    if (size == null) {
-      // Unable to get width of anchored banner.
-      return;
-    }
-
-    BannerAd(
-      adUnitId: _adUnitId,
-      request: const AdRequest(),
-      size: size,
-      listener: BannerAdListener(
-        // Called when an ad is successfully received.
-        onAdLoaded: (ad) {
-          // setState(() {
-          //   _bannerAd2 = ad as BannerAd;
-          //   _isLoaded2 = true;
-          // });
-        },
-        // Called when an ad request failed.
-        onAdFailedToLoad: (ad, err) {
-          ad.dispose();
-        },
-        // Called when an ad opens an overlay that covers the screen.
-        onAdOpened: (Ad ad) {},
-        // Called when an ad removes an overlay that covers the screen.
-        onAdClosed: (Ad ad) {},
-        // Called when an impression occurs on the ad.
-        onAdImpression: (Ad ad) {},
-      ),
-    ).load();
-  }
-
-  void _loadAd1() async {
-    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
-    final size = await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
-        MediaQuery.of(context).size.width.truncate());
-
-    if (size == null) {
-      // Unable to get width of anchored banner.
-      return;
-    }
-
-    BannerAd(
-      adUnitId: _adUnitId,
-      request: const AdRequest(),
-      size: size,
-      listener: BannerAdListener(
-        // Called when an ad is successfully received.
-        onAdLoaded: (ad) {
-          // setState(() {
-          //   _bannerAd1 = ad as BannerAd;
-          //   _isLoaded1 = true;
-          // });
-        },
-        // Called when an ad request failed.
-        onAdFailedToLoad: (ad, err) {
-          ad.dispose();
-        },
-        // Called when an ad opens an overlay that covers the screen.
-        onAdOpened: (Ad ad) {},
-        // Called when an ad removes an overlay that covers the screen.
-        onAdClosed: (Ad ad) {},
-        // Called when an impression occurs on the ad.
-        onAdImpression: (Ad ad) {},
-      ),
-    ).load();
-  }
-
-  @override
-  void dispose() {
-    // _bannerAd2?.dispose();
-    // _bannerAd1?.dispose();
-    super.dispose();
   }
 }
